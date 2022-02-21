405cf9416abaf98ef0c95c98fe34efb9cd430359
Jorge Vivas <slacksky@gmail.com>
2022-02-21 13:28:04 -0500

    initial search bar setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { DemoComponent } from './demo/demo.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [  
+  {
+  path: 'searchbar',
+  component: SearchbarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { DemoComponent } from './demo/demo.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    DemoComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
